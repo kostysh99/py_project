6b68b03eec241b234dd5199f60c6aacef1e88955
kostysh <kostysh99@yandex.ru>
2024-08-16 21:26:15 +0700

    fourth commit

--- py_project/main.py
+++ py_project/main.py
@@ -1,6 +1,7 @@
 one_number = 5
 two_number = 3
 sum_numbers = 0
+subtracting_nombers = 0
 
 
 def sum_of_two_numbers():
@@ -8,4 +9,10 @@ def sum_of_two_numbers():
     return sum_numbers
 
 
+def subtracting_two_numbers():
+    subtracting_nombers = one_number - two_number
+    return subtracting_nombers
+
+
 print(sum_of_two_numbers())
+print(subtracting_two_numbers())
